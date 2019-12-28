663b3ed86aa2b47834c6b8d3f57407ed05b90751
Shashanka Panuganti <pshashanka@gmail.com>
2019-12-28 00:13:49 -0800

    Create anagramMaker.ts

---
+++ anagram/anagramMaker.ts
@@ -0,0 +1,45 @@
+function anagramMaker(str: string): string[]{
+    let map = new Map<string, number>()
+    for (const char of str) {
+        let counter = map.get(char) || 1;
+        map.set(char, counter)
+  }
+
+  let counter = str.length;  
+
+    const helper = function (input: string) : string[] {
+      // ab - ab, ba
+
+        if (input.length < 2) {
+            return [input]
+        }
+
+        if (input.length == 2) {
+          return [input.charAt(0) + input.charAt(1), input.charAt(1) + input.charAt(0)]
+      }
+
+      let output : string[] = []
+      for (let i = 0; i < counter && i < input.length; i++) {
+        const char = input.charAt(0);
+        input = input.slice(1)
+        const temp = helper(input)
+        console.log(input, temp, char)
+        
+        output.push(...temp.map(item => char + item)) 
+        console.log('output', output) 
+        input += char      
+      }   
+
+
+        
+        return output
+
+    }
+
+    const results = helper(str)
+    return results;
+}
+
+
+
+console.log(anagramMaker('abcd'))
