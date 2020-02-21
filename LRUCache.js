b27859da4870a1cc8632be93541b68331c7d8a24
Shashanka Panuganti <pshashanka@gmail.com>
2020-02-21 11:20:16 -0800

    Update LRUCache.js

--- LRUCache.js
+++ LRUCache.js
@@ -1,59 +1,101 @@
-/**
- * @param {number} capacity
- */
-var LRUCache = function(capacity) {
-    this.size = capacity;
-    this.cache = {}
-    this.accessCounter = {}
-    this.counter = 0;
-};
-
-/** 
- * @param {number} key
- * @return {number}
- */
-LRUCache.prototype.get = function(key) {
-    if(this.cache.hasOwnProperty(key)) {
-        this.accessCounter[key] = this.counter++;
-        return this.cache[key]
+class Node {
+    constructor(key=null, val = null) {
+        this.key = key;
+        this.val = val;
+        this.next = null;
+        this.prev = null;
     }
     
-    return -1;
-};
-
-LRUCache.prototype.ensureCapacity = function() {
-    
-    const keys = Object.keys(this.cache)
-
-    if(keys.length < this.size) {
-        return;
+    isEqual(node) {
+        return (!node && this.key === node.key)
     }
     
-    const sorted = keys.sort((a,b) => this.accessCounter[a] - this.accessCounter[b]) 
-    const keyToDelete = sorted[0];
-    delete this.cache[keyToDelete];
-    delete this.accessCounter[keyToDelete];
+    toString = () => ('key :' + this.key + ', value: ' + this.val);
 }
 
-/** 
- * @param {number} key 
- * @param {number} value
- * @return {void}
- */
-LRUCache.prototype.put = function(key, value) {
+class DList {
+    constructor() {
+        this.head = new Node('HEAD');
+        this.tail = new Node('TAIL');
+        this.head.next = this.tail;
+        this.tail.prev = this.head;
+    }
     
-    if(!this.cache.hasOwnProperty(key)) {
-       this.ensureCapacity();
+    put(key, val) {
+       const node = new Node(key, val);
+       this.adjustNode(node);
+       return node;
+    } 
+    
+    adjustNode(node) {
+        
+        if(node.prev) {
+            node.prev.next = node.next;
+        }
+        
+        if(node.next) {
+            node.next.prev = node.prev;
+        }
+        
+        node.next = this.tail;
+        node.prev = this.tail.prev;
+        
+        this.tail.prev.next = node;
+        this.tail.prev = node;
+        console.log(' now ', this.head.next.toString())
+        return node;
+    }
+    
+    deleteLast() {
+        const node = this.head.next;
+        this.head.next = node.next;
+        node.next.prev = this.head;
+        return node.key;
     }
     
     
-    this.cache[key] = value;
-    this.accessCounter[key] = this.counter++;
-};
+}
 
-/** 
- * Your LRUCache object will be instantiated and called as such:
- * var obj = Object.create(LRUCache).createNew(capacity)
- * var param_1 = obj.get(key)
- * obj.put(key,value)
- */
+class LRUCache {
+    constructor(capacity) {
+        this.capacity = capacity;
+        this.nodeMap = new Map();
+        this.nodeList = new DList();
+        this.count = 0;
+    }
+    
+    get(key) {
+        console.log('get', key)
+        if(this.nodeMap.has(key)) {
+            let node = this.nodeMap.get(key);
+            node = this.nodeList.adjustNode(node);
+            this.nodeMap.set(key, node);
+            return node.val;
+        }
+        
+        return -1;
+    }
+    
+    put(key, value) {
+        console.log('put', key)
+        
+        if(this.nodeMap.has(key)) {
+            let node = this.nodeMap.get(key);
+            node.val = value;
+            node = this.nodeList.adjustNode(node);
+            this.nodeMap.set(key, node);
+            return;
+        }        
+        
+        if(this.count >= this.capacity) {
+            const key = this.nodeList.deleteLast();
+            console.log(key , ' deleting')
+            this.nodeMap.delete(key);
+            --this.count;
+        }
+
+        const node = this.nodeList.put(key, value);
+        this.nodeMap.set(key, node);
+        ++this.count;        
+    }
+}
